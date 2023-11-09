e157dc92bdcccf78ece4c24d216cf157fef38015
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-09 17:33:05 +0100

    17.32

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function creaGriglia() {
 
     let cellePerRiga = Math.sqrt(numeroCelletot);
     console.log("numero celle per riga" , cellePerRiga);
+    //let quadratino = creaQuadratino();
     //quadratino.style.width = `calc (100% / ${cellePerRiga})`;
     //quadratino.style.height = `calc (100% / ${cellePerRiga})`;
 
@@ -31,29 +32,39 @@ function creaGriglia() {
     
     for (let i = 1; i <= numeroCelletot ; i++) {
         
-        let cella = creaQuadratino(i);
+        //let stringaCella = creaQuadratino(i);
+        let cellaSingola = creaQuadratino(i);
     
         // calcoloHew ();  
+        const vediamo = `calc (100% / ${cellePerRiga} )`;
+        console.log(vediamo , "RISULTATO CALC");
 
-        cella.style.width = `calc (100% / ${cellePerRiga})`;
-        cella.style.height = `calc (100% / ${cellePerRiga})`;
+        cellaSingola.style.width =  "50px";//`calc (100% / ${cellePerRiga} )`;
+        cellaSingola.style.height = "50px";// `calc (100% / ${cellePerRiga} )`;
 
-        gridBuild.appendChild(cella);
+        gridBuild.appendChild(cellaSingola);
     }     
 }
 
 function creaQuadratino(indexCell) {
     let quadratino = document.createElement("div");
         quadratino.classList.add("square");
-        
-        //quadratino.innerHTML += `${indexCell}`
+
+        /*const numeroCelletot = livelloDifficoltà(); 
+         console.log("numero celle totali" , numeroCelletot);
+
+         let cellePerRiga = Math.sqrt(numeroCelletot);
+         console.log("numero celle per riga" , cellePerRiga);*/
+       
         quadratino.innerText = (indexCell);
+        //quadratino.style.width += `calc (100% / ${cellePerRiga} )`;
+        //quadratino.style.height += `calc (100% / ${cellePerRiga} )`;
         quadratino.addEventListener("click" , function () {
             this.classList.toggle("black");
             console.log("valore della cella cliccata è" , indexCell);  
             quadratino.classList.toggle("stylenumber");
         })
-    return quadratino;    
+    return quadratino; 
 }
 
 

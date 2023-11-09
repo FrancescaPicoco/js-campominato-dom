230160a6623925eac286b026c4467ad3c84134f6
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-09 11:04:52 +0100

    9/11 11.04

--- main.js
+++ main.js
@@ -2,29 +2,76 @@
 document.getElementById("btnPlayer").addEventListener("click" , function(){
     creaGriglia();
 })   
+function livelloDifficoltà () {
+    let level = parseInt(document.getElementById("difficolta").value);
+    console.log(level);
+    let numeroCelletot = 100;
+    if (level == 2){
+       numeroCelletot=81;
+    }
+    else if (level == 3){
+        numeroCelletot=49;
+    }
+    return numeroCelletot;
+}
 
-function creaGriglia() {
-    let numeroCelle = 100
+function creaGriglia() {  
     const gridBuild = document.getElementById("grid");
     gridBuild.classList.add("griglia");
-    //gridBuild.innerHTML = "";
-    for (let i = 1; i <= numeroCelle ; i++) {
-        let indexCell = creaQuadratino(i);
-        creaQuadratino(indexCell)
-        console.log(indexCell);
-        gridBuild.appendChild(indexCell);
-    }    
+
+    const numeroCelletot = livelloDifficoltà(); 
+    console.log("numero celle totali" , numeroCelletot);
+
+    let cellePerRiga = Math.sqrt(numeroCelletot);
+    console.log("numero celle per riga" , cellePerRiga);
+    //quadratino.style.width = `calc (100% / ${cellePerRiga})`;
+    //quadratino.style.height = `calc (100% / ${cellePerRiga})`;
+
+    gridBuild.innerHTML = ""; 
+    
+    for (let i = 1; i <= numeroCelletot ; i++) {
+        
+        let cella = creaQuadratino(i);
+    
+        // calcoloHew ();  
+
+        cella.style.width = `calc (100% / ${cellePerRiga})`;
+        cella.style.height = `calc (100% / ${cellePerRiga})`;
+
+        gridBuild.appendChild(cella);
+    }     
 }
 
 function creaQuadratino(indexCell) {
     let quadratino = document.createElement("div");
         quadratino.classList.add("square");
+        
         //quadratino.innerHTML += `${indexCell}`
         quadratino.innerText = (indexCell);
         quadratino.addEventListener("click" , function () {
             this.classList.toggle("black");
-            console.log(indexCell)  
+            console.log("valore della cella cliccata è" , indexCell);  
             quadratino.classList.toggle("stylenumber");
         })
     return quadratino;    
 }
+
+
+//calcoloHeW();
+//let numeroCelletot;
+//let cellePerRiga;
+
+/*function calcoloHeW () {
+    numeroCelletot = livelloDifficoltà();
+    console.log("numero totale delle celle" , numeroCelletot);
+    let cellePerRiga = Math.sqrt(numeroCelletot);
+    console.log("numero celle per riga" , cellePerRiga);
+    for (let i = 0; i < numeroCelletot; i++) {
+       let cellaHeW = creaQuadratino(i);
+        cellaHeW.style.height = `calc (100% / ${cellePerRiga})`;
+        cellaHeW.style.width = `calc (100% / ${cellePerRiga})`;
+        
+    }
+}*/
+
+
